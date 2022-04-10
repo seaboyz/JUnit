5d51a01e1184bee90c89b27eef1321a1e1905fb6
seaboyz <seaboyz@hotmail.com>
2022-04-10 05:50:45 -0500

    Add first test for add(int a, int b)

---
+++ junit/src/main/java/com/seaboyz/MathUtils.java
@@ -0,0 +1,7 @@
+package com.seaboyz;
+
+public class MathUtils {
+  public int add(int a, int b) {
+    return a + b;
+  }
+}
---
+++ junit/src/test/java/com/seaboyz/MathUtilsTest.java
@@ -0,0 +1,13 @@
+package com.seaboyz;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MathUtilsTest {
+  @Test
+  void test() {
+    MathUtils mathUtils = new MathUtils();
+    assertEquals(mathUtils.add(1, 1), 1, "The add method should add two numbers");
+  }
+}
