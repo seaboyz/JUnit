9d4ee2d7e4e9c9386e6d31b4e23f5ed350bf8db7
seaboyz <seaboyz@hotmail.com>
2022-04-10 08:16:22 -0500

    Add test lifecyle @beforeEach init()

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 #### What is testing?
 
 ![](./images/download.png)
+
+#### Test life cycle
+
+![](./images/Screen%20Shot%202022-04-10%20at%208.08.03%20AM.png)
+![](./images/Screen%20Shot%202022-04-10%20at%208.12.46%20AM.png)
--- junit/src/test/java/com/seaboyz/MathUtilsTest.java
+++ junit/src/test/java/com/seaboyz/MathUtilsTest.java
@@ -4,31 +4,35 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MathUtilsTest {
+  MathUtils mathUtils;
+
+  @BeforeEach
+  void init() {
+    mathUtils = new MathUtils();
+  }
+
   @Test
   void testAdd() {
-    MathUtils mathUtils = new MathUtils();
     assertEquals(2, mathUtils.add(1, 1), "The add method should add two numbers");
   }
 
   @Test
   void testMultiply() {
-    MathUtils mathUtils = new MathUtils();
     assertEquals(4, mathUtils.multiply(2, 2), "The multiply mehod should multiply two numbers");
   }
 
   @Test
   void testComputeCircleArea() {
-    MathUtils mathUtils = new MathUtils();
     int r = anyInt();
     assertEquals(Math.PI * r * r, mathUtils.computeCircleArea(r));
   }
 
   @Test
   void testDivideThrowExceptionWhenPassZeroAsDemoninator() {
-    MathUtils mathUtils = new MathUtils();
     assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0));
   }
 }
